145a81ab621d9d5b350583fec5e97be702a3f887
qr-github <umeda.qr0813@gmail.com>
2026-08-17 01:39:56 +0900

    IO，表示ロジックを作成

--- static/rogic.js
+++ static/rogic.js
@@ -12,3 +12,26 @@ fetch('/extract', {
     console.error('エラー：', err);
     document.getElementById('output_section').textContent = 'Error!! :' + err;
 });
+
+const copy_btn = document.getElementById('for_copy');
+const input_form = document.querySelector('textarea[name="input_form"]');
+const toast = document.getElementById('toast');
+
+function showToast(){
+    toast.classList.remove('hidden');
+    toast.classList.add('show');
+
+    setTimeout(()=>{
+        toast.classList.remove('show');
+        toast.classList.add('hidden');
+    }, 2000);
+}
+
+copy_btn.addEventListener('click', ()=>{
+    const textToCopy = input_form.value;
+
+    navigation.clipboard.writeText(textToCopy)
+    .then(()=>{
+        showToast();
+    });
+});
